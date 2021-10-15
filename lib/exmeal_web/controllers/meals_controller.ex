92e2e8b38a2ee5a9855a57fa4e3be4c8921c2e8a
Renato Marinho <renato.marinhofr@gmail.com>
2021-10-15 09:37:35 -0300

    create endpoint update meal by id

--- lib/exmeal_web/controllers/meals_controller.ex
+++ lib/exmeal_web/controllers/meals_controller.ex
@@ -29,4 +29,12 @@ defmodule ExmealWeb.MealsController do
       |> render("meal.json", meal: meal)
     end
   end
+
+  def update(conn, params) do
+    with {:ok, %Meal{} = meal} <- Exmeal.update_meal(params) do
+      conn
+      |> put_status(:ok)
+      |> render("meal.json", meal: meal)
+    end
+  end
 end
